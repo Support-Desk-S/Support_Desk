4d48c2e24814d3807a9aaa87b262ed3cf578b2e0
Rohit Mahajan <rohitmahajan1845@gmail.com>
2026-05-03 23:59:29 +0530

    fix: correct formatting of Toaster component in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <ConfirmProvider>
         <App />
-        <Toaster />``
+        <Toaster />
       </ConfirmProvider>
     </BrowserRouter>
   </Provider>,
